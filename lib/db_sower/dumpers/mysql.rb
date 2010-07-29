8d58cb4a3552baafdf4e71c7cd89732d06301135
hallelujah <hery@rails-royce.org>
2010-07-29 15:35:47 +0200

    Add data dumper class

---
+++ lib/db_sower/dumpers/mysql.rb
@@ -0,0 +1,50 @@
+module DbSower
+  module Dumpers
+    class Mysql
+
+      attr_reader :config, :options, :command, :ident, :conditions, :dest
+      def initialize(ident,args = {})
+        @ident, @options = ident, args
+        @conditions = @options.delete(:conditions)
+        @dest = @options.delete(:to)
+        @config = ident.to_hash
+        @command = @config[:command] || '/usr/bin/mysqldump'
+        @config.delete_if{|k,v| v.nil? || v.respond_to?(:empty?) && v.empty? || ![:database, :host, :user, :password, :socket, :port].include?(k)}
+      end
+
+      def execute
+        system(command,*dump_options)
+      end
+
+      # keys available : table
+      def generate_credential_options
+        config.map do |k,v|
+          ["--#{k}","#{v}"]
+        end.sort
+      end
+
+      def generate_conditions
+        return [] if conditions.nil? || conditions.empty?
+        ["--where",%Q{"#{conditions}"}]
+      end
+
+      def generate_mysqldump_options
+        a = options.map do |k,v|
+          case v
+          when nil
+            ["--#{k}"]
+          else
+            ["--#{k}","#{v}"]
+          end
+        end
+        a << ['--result-file',dest] unless dest.nil? || dest.empty?
+        a
+      end
+
+      def dump_options
+        (generate_credential_options + generate_conditions + generate_mysqldump_options).map{|a| a.join(' ')}
+      end
+
+    end
+  end
+end
---
+++ test/db_sower/test_mysql_dumper.rb
@@ -0,0 +1,39 @@
+require 'helper'
+class TestMysqlDumper < Test::Unit::TestCase
+
+  def setup
+    h = ActiveRecord::Base.configurations['db1']
+    ident = DbSower::DumpBackend::Identifier.new(h)
+    @dumper1 = DbSower::Dumpers::Mysql.new(ident,{:table => :creations, :conditions => "id=1"})
+    @dumper2 = DbSower::Dumpers::Mysql.new(ident,{:table => :creations, :xml => nil})
+    @dumper3 = DbSower::Dumpers::Mysql.new(ident,{:table => :creations, :to => 'filename.sql'})
+  end
+
+  def test_generate_conditions
+    assert_equal ["--where",'"id=1"'], @dumper1.generate_conditions
+    assert_equal [], @dumper2.generate_conditions
+  end
+
+  def test_generate_credential_options
+    credentials = [
+      ["--database",'database1'],
+      ['--host','localhost'],
+      ['--password','db_sower'],
+      ['--port','3306'],
+      ['--socket','/var/run/mysqld/mysqld.sock'],
+      ['--user','db_sower']
+    ]
+    assert_equal credentials, @dumper1.generate_credential_options
+    assert_equal credentials, @dumper2.generate_credential_options
+  end
+
+  def test_generate_mysql_dump_options
+    assert_equal [["--table",'creations']], @dumper1.generate_mysqldump_options
+    assert_equal [["--table",'creations'],["--xml"]], @dumper2.generate_mysqldump_options
+  end
+
+  def test_dump_options
+    assert_equal ["--database database1","--host localhost", "--password db_sower", "--port 3306", "--socket /var/run/mysqld/mysqld.sock", "--user db_sower", "--table creations", "--result-file filename.sql"], @dumper3.dump_options
+
+  end
+end
