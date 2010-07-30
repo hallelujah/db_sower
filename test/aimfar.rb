059412d5faaf4c246c5815584f082943c20e790c
hallelujah <hery@rails-royce.org>
2010-07-30 13:26:29 +0200

    A manual test for aimfar_prod

---
+++ test/aimfar.rb
@@ -0,0 +1,39 @@
+$:.unshift(File.expand_path('../../lib',__FILE__))
+require 'rubygems'
+require 'active_record'
+require 'db_sower'
+
+seed = DbSower::Seed.new 
+
+ActiveRecord::Base.configurations = {
+  :aimfar_prod => 
+  {
+    :database => 'aimfar_prod',
+    :socket => '/tmp/webo-mysql-stat.sock',
+    :username => 'root',
+    :password => '',
+    :host => 'localhost'
+  }
+}.with_indifferent_access
+
+seed.graft(:identifier => :aimfar_prod) do 
+  clients.where(:id => 4)
+  users.with(:clients).where(:client_id => :id)
+  achats.where(:status => [-1,1]).with(:clients).where(:client_id => :id)
+  campagnes.with(:achats).where(:id => :campagne_id)
+  creations.with(:achats).where(:achat_id => :id)
+  ciblages
+end
+ident = DbSower::DumpBackend::Identifier.new(ActiveRecord::Base.configurations[:aimfar_prod])
+
+backend = DbSower::DumpBackend.new(seed,File.expand_path('../aimfar.yml',__FILE__))
+
+seed.tsort.each do |node|
+  identifier = node.options[:identifier]
+  database = backend.config[identifier][:database]
+  dumper = DbSower::Dumpers::Mysql.new(ident, :table => node.table_name, :conditions => backend.adapter(node).formatted_conditions(backend), :to => "#{database}.sql", :append => true)
+#  puts dumper.command_line.join(' ')
+  puts "Dumping #{database}.#{node.table_name}"
+  dumper.execute
+end
+
