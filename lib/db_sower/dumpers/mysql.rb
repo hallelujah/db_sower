82f6cbd0904f9dce5a2d2a941177451c81fd3a5e
hallelujah <hery@rails-royce.org>
2010-07-30 11:31:10 +0200

    Fix credentials, quoting in dumpers
    * mysqldump credentials requires no option for database and creations
    * mysqldump conditions --where option must be simple quoted on command line

--- lib/db_sower/dumpers/mysql.rb
+++ lib/db_sower/dumpers/mysql.rb
@@ -2,36 +2,45 @@ module DbSower
   module Dumpers
     class Mysql
 
-      attr_reader :config, :options, :command, :ident, :conditions, :dest
+      attr_reader :config, :options, :command, :ident, :dest
+      attr_accessor :conditions
       def initialize(ident,args = {})
-        @ident, @options = ident, args
+        @ident, @options = ident.clone, args.clone
         @conditions = @options.delete(:conditions)
         @dest = @options.delete(:to)
-        @config = ident.to_hash
+        @config = ident.to_hash.clone
         @command = @config[:command] || '/usr/bin/mysqldump'
-        @config.delete_if{|k,v| v.nil? || v.respond_to?(:empty?) && v.empty? || ![:database, :host, :user, :password, :socket, :port].include?(k)}
+        @database = @config.delete(:database)
+        @table = @options.delete(:table).to_s
+        @config.delete_if{|k,v| v.nil? || v.respond_to?(:empty?) && v.empty? || ![ :host, :user, :password, :socket, :port].include?(k)}
       end
 
       def execute
-        system(command,*dump_options)
+        system(*command_line)
+      end
+
+      def command_line
+        [command,*dump_options]
       end
 
       # keys available : table
       def generate_credential_options
-        config.map do |k,v|
-          ["--#{k}","#{v}"]
+        a = config.map do |k,v|
+          ["--#{k}=#{v}"]
         end.sort
+        a += [[@database],[@table]]
+        a
       end
 
       def generate_conditions
         return [] if conditions.nil? || conditions.empty?
-        ["--where",%Q{"#{conditions}"}]
+        [["--where",%Q{'#{conditions}'}]]
       end
 
       def generate_mysqldump_options
         a = options.map do |k,v|
           case v
-          when nil
+          when true
             ["--#{k}"]
           else
             ["--#{k}","#{v}"]
@@ -42,7 +51,7 @@ module DbSower
       end
 
       def dump_options
-        (generate_credential_options + generate_conditions + generate_mysqldump_options).map{|a| a.join(' ')}
+        (generate_credential_options + generate_mysqldump_options + generate_conditions ).map{|a| a.join(' ')}
       end
 
     end
--- test/db_sower/test_mysql_dumper.rb
+++ test/db_sower/test_mysql_dumper.rb
@@ -1,39 +1,63 @@
 require 'helper'
 class TestMysqlDumper < Test::Unit::TestCase
+  fixtures :creations, :masques
 
   def setup
     h = ActiveRecord::Base.configurations['db1']
     ident = DbSower::DumpBackend::Identifier.new(h)
-    @dumper1 = DbSower::Dumpers::Mysql.new(ident,{:table => :creations, :conditions => "id=1"})
-    @dumper2 = DbSower::Dumpers::Mysql.new(ident,{:table => :creations, :xml => nil})
-    @dumper3 = DbSower::Dumpers::Mysql.new(ident,{:table => :creations, :to => 'filename.sql'})
+    @dumper1 = DbSower::Dumpers::Mysql.new(ident,{:table => :masques, :conditions => "id=1"})
+    @dumper2 = DbSower::Dumpers::Mysql.new(ident,{:table => :masques, :xml => true})
+    @dumper3 = DbSower::Dumpers::Mysql.new(ident,{:table => :masques, :to => 'filename.sql'})
+    @seed = DbSower::Seed.new
+    @seed.graft(:identifier => :db1) do
+      creations
+      masques.with(:creations).where(:id => :masque_id)
+    end
+    @db_backend = DbSower::DumpBackend.new(@seed, File.expand_path('config/db.yml',$test_root))
+    @creation = DbSower::Node.new(@seed,:creations)
+    @masque = DbSower::Node.new(@seed,:masques)
+    @edge = @seed.edge(@masque,@creation)
+    @c1 = creations(:creation1) 
+    @c2 = creations(:creation2) 
+    @m1 = masques(:masque1) 
+    @m2 = masques(:masque2) 
+    @c1.masque = @m1
+    @c2.masque = @m2
+    @c1.save!
+    @c2.save!
   end
 
   def test_generate_conditions
-    assert_equal ["--where",'"id=1"'], @dumper1.generate_conditions
+    assert_equal [["--where","'id=1'"]], @dumper1.generate_conditions
     assert_equal [], @dumper2.generate_conditions
   end
 
   def test_generate_credential_options
     credentials = [
-      ["--database",'database1'],
-      ['--host','localhost'],
-      ['--password','db_sower'],
-      ['--port','3306'],
-      ['--socket','/var/run/mysqld/mysqld.sock'],
-      ['--user','db_sower']
+      ["--host=localhost"],
+      ['--password=db_sower'],
+      ['--port=3306'],
+      ['--socket=/var/run/mysqld/mysqld.sock'],
+      ['--user=db_sower'],
+      ["database1"],
+      ["masques"]
     ]
     assert_equal credentials, @dumper1.generate_credential_options
     assert_equal credentials, @dumper2.generate_credential_options
+    assert_equal credentials, @dumper3.generate_credential_options
   end
 
   def test_generate_mysql_dump_options
-    assert_equal [["--table",'creations']], @dumper1.generate_mysqldump_options
-    assert_equal [["--table",'creations'],["--xml"]], @dumper2.generate_mysqldump_options
+    assert_equal [], @dumper1.generate_mysqldump_options
+    assert_equal [["--xml"]], @dumper2.generate_mysqldump_options
   end
 
   def test_dump_options
-    assert_equal ["--database database1","--host localhost", "--password db_sower", "--port 3306", "--socket /var/run/mysqld/mysqld.sock", "--user db_sower", "--result-file filename.sql", "--table creations"], @dumper3.dump_options
+    assert_equal ["--host=localhost", "--password=db_sower", "--port=3306", "--socket=/var/run/mysqld/mysqld.sock", "--user=db_sower", "database1", "masques", "--result-file filename.sql"], @dumper3.dump_options
+  end
 
+  def test_command_line
+    @dumper3.conditions = @db_backend.adapter(@masque).formatted_conditions(@db_backend)
+    assert_equal ["/usr/bin/mysqldump", "--host=localhost", "--password=db_sower", "--port=3306", "--socket=/var/run/mysqld/mysqld.sock", "--user=db_sower", "database1","masques", "--result-file filename.sql", "--where '(`masques`.`id` IN ('1','2'))'"], @dumper3.command_line
   end
 end
