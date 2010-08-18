6016bedf34b0ea5b434a64bbeee89a1a0f880bcb
hallelujah <hery@rails-royce.org>
2010-08-18 18:08:40 +0200

    Refactor condition and add_condition method + doc

--- lib/sower/condition.rb
+++ lib/sower/condition.rb
@@ -1,7 +1,26 @@
 module Sower
+  # This class provides all the logic to link nodes, edges, etc ...
   class Condition
 
+    # This module helps on adding some useful methods to classes with conditional aspects.
+    module Methods
+      ##
+      # :attr_reader: condition
+      define_method :condition do
+        @condition ||= Condition.new(nil)
+      end
+
+      # Append a condition purpose to condition
+      def add_condition!(cond)
+        condition << condition
+      end
+    end
+
     attr_reader :values
+
+    # You can use any Object which implements to_condition
+    #   Sower::Condition.new(Object)
+    #
     def initialize(condition)
       @values = [condition]
     end
