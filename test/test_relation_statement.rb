8abc4b526bb05a85cb1a2e245766b694d7e94c60
hallelujah <hery@rails-royce.org>
2010-08-24 18:12:17 +0200

    make tests run for relation

--- test/test_relation_statement.rb
+++ test/test_relation_statement.rb
@@ -20,7 +20,7 @@ class TestRelationStatement < Test::Unit::TestCase
     should "return a query String when sent #to_sql" do
     statement = @statement.where(@users[:id].ne(1).and(@users[:status].eq(1)))
     sql = statement.or(@users[:login].like("su%")).to_sql
-    assert_equal "((`users`.`name` IN ('john','edward')) AND ((`users`.`id` != '1') AND (`users`.`status` = '1'))) OR (`users`.`login` LIKE 'su%')", sql
+    assert_equal "((`users`.`name` IN ('john','edward')) \nAND ((`users`.`id` != '1') \nAND (`users`.`status` = '1'))) \nOR (`users`.`login` LIKE 'su%')", sql
     end
   end
 end
