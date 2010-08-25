d24a831e982ffbe7ed54eacdc7c59f4b8560134e
hallelujah <hery@rails-royce.org>
2010-08-25 18:19:01 +0200

    Add #attributes method to Relation::Statement + doc

--- lib/sower/relation/statement.rb
+++ lib/sower/relation/statement.rb
@@ -1,25 +1,46 @@
 module Sower
   module Relation
     class Statement
+      # Initialialize a statement where attribute and values are used between the connector
       def initialize(attribute,value)
         @attribute = attribute
         @value = value
       end
 
+      # Create a new AndStatement with self as attribute and statement as value
+      # It is a simple chaining
       def where(statement)
         AndStatement.new(self, statement)
       end
       alias and where
 
+      # Create a new OrStatement with self as attribute and statement as value
+      # It is a simple chaining
       def or(statement)
         OrStatement.new(self,statement)
       end
 
+      # Generate a SQL String well formatted
+      # TODO SQL string must be handled by a SQL formatter ... no matter for now
       def to_sql
         [@attribute.to_sql,@value.to_sql].compact.join(' ')
       end
 
+      # Return a Hash of attributes indexed by table
+      def attributes
+        [@attribute,@value].inject(Hash.new([])) do |memo,c|
+          case c 
+          when Sower::Relation::Statement
+            memo.merge!(c.attributes){|k,v,n| v | n}
+          when Sower::Relation::Attribute
+            memo[c.table] |= [c]
+          end
+          memo
+        end
+      end
+
       protected
+      # Generate brackets around the value of the yielded block
       def paren
         res = yield.to_s.strip
         res.empty? ? nil : "(#{res})"
@@ -28,22 +49,30 @@ module Sower
     end
 
     class AndStatement < Statement
+      # Generate a SQL String well formatted
+      # TODO SQL string must be handled by a SQL formatter ... no matter for now
       def to_sql
         [paren{@attribute.to_sql},paren{@value.to_sql}].compact.join(" \nAND " )
       end
     end
 
     class OrStatement < Statement
+      # Generate a SQL String well formatted
+      # TODO SQL string must be handled by a SQL formatter ... no matter for now
       def to_sql
         "(#{@attribute.to_sql}) \nOR (#{@value.to_sql})"
       end
     end
 
     module Stateable
+      ##
+      # :attr_reader: statements
       def statements
         @statements
       end
 
+      # Generate a new Statement or AndStatement depending on statement.is_nil? and set statement to it.
+      # Return self
       def where(statement)
         if statements.nil?
           @statements = Sower::Relation::Statement.new(@statements,statement)
@@ -53,16 +82,21 @@ module Sower
         self
       end
 
+      # Generate a new AndStatement and set statement to it.
+      # Return self
       def and(statement)
         @statements = Sower::Relation::AndStatement.new(@statements,statement)
         self
       end
 
+      # Generate a new OrStatement and set statement to it.
+      # Return self
       def or(statement)
         @statements = Sower::Relation::OrStatement.new(@statements,statement)
         self
       end
 
+      # Just return a nil value when invoking #to_sql for Stateable objects.
       def to_sql
         nil
       end
--- test/test_relation_statement.rb
+++ test/test_relation_statement.rb
@@ -19,8 +19,20 @@ class TestRelationStatement < Test::Unit::TestCase
 
     should "return a query String when sent #to_sql" do
       statement = @statement.where(@users[:id].ne(1).and(@users[:status].eq(1)))
+      assert_kind_of Sower::Relation::Statement, statement
       sql = statement.or(@users[:login].like("su%")).to_sql
       assert_equal "((`users`.`name` IN ('john','edward')) \nAND ((`users`.`id` != '1') \nAND (`users`.`status` = '1'))) \nOR (`users`.`login` LIKE 'su%')", sql
+    end
+
+    should "respond to attributes" do
+      assert_respond_to @statement, :attributes
+    end
+
+    should "return a Hash which keys are Relation::Table and values Relation::Attibute when sent #attributes" do
+      assert_instance_of Hash, @statement.attributes
+      statement = @statement.where(@users[:id].ne(1).and(@users[:status].eq(1)))
+      assert statement.attributes.any?
+      assert statement.attributes.all?{|k,v| k.is_a?(Sower::Relation::Table) && v.all?{|a| a.is_a?(Sower::Relation::Attribute)} }
     end
 
   end
